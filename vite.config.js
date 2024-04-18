9e739d7de5e9cec673c746cfe1c418a0e9dfe7b5
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@provider.pl>
2024-04-18 11:10:11 +0200

    chore: remove phpStorm warnings

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { fileURLToPath } from 'url'
-import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 
 // https://vitejs.dev/config/
+// noinspection JSUnusedGlobalSymbols
 export default defineConfig({
   plugins: [react()],
   resolve: {
